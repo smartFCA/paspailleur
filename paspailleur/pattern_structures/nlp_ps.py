47014c22a634735916cab57d4e01c098c829e545
Egor Dudyrev <egor.dudyrev@loria.fr>
2023-08-30 19:21:29 +0200

    Refactor NgramsPS (except for iter_bin_attributes)

--- paspailleur/pattern_structures/nlp_ps.py
+++ paspailleur/pattern_structures/nlp_ps.py
@@ -1,44 +1,57 @@
-from typing import Iterator, Optional
+import difflib
+from typing import Iterator
+
 from bitarray import frozenbitarray as fbarray
 from bitarray import bitarray
 from paspailleur.pattern_structures import AbstractPS
+
+import nltk
 from nltk.util import ngrams
 from nltk.corpus import wordnet
 nltk.download('wordnet')
 
+
 class NgramsPS(AbstractPS):
-    PatternType =  set[tuple[str]]
-    bottom = None
-    min_n : int
+    PatternType = set[tuple[str]]  # Every tuple represents an ngram of words. A pattern is a set of incomparable ngrams
+    bottom: PatternType = None  # the most specific ngram for the data. Equals to None for simplicity
+    min_n: int = 1  # Minimal size of an ngram to consider
     
-    def __init__(self, min_n = 1):
+    def __init__(self, min_n: int = 1):
         self.min_n = min_n
 
     def join_patterns(self, a: PatternType, b: PatternType) -> PatternType:
-        """Return the common ngrams between both patterns `a` and `b`"""
+        """Return the maximal sub-ngrams contained both in `a` and in `b`
 
+        Example:
+        >> join_patterns({('hello', 'world'), ('who', 'is', 'there')}, {('hello', 'there')}) == {('hello',), ('there',)}
+        as 'hello' is contained both in 'hello world' and 'hello there'
+        and 'there' is contained both in 'who is there' and 'hello there'
+        """
         # Transform the set of words into text
-        tuple_text1 = list(a)[0]
-        tuple_text2 = list(b)[0]
-        text1 = ' '.join(tuple_text1)
-        text2 = ' '.join(tuple_text2)
+        seq_matcher = difflib.SequenceMatcher(isjunk=lambda ngram: len(ngram) < self.min_n)
 
-        # Find identical n-grams for all possible sizes
-        identical_ngrams = set()
-        max_n = min(len(text1.split()), len(text2.split()))
-        for n in range(self.min_n, max_n + 1):
-            ngrams1 = set(ngrams(text1.split(), n))
-            ngrams2 = set(ngrams(text2.split(), n))
-            identical_ngrams.update(ngrams1.intersection(ngrams2))
+        common_ngrams = []
+        for ngram_a in a:
+            seq_matcher.set_seq1(ngram_a)
+            for ngram_b in b:
+                seq_matcher.set_seq2(ngram_b)
 
-        # Delete identical n-grams contained in other identical n-grams
-        for ngram in list(identical_ngrams):
-            for other_ngram in identical_ngrams:
-                if ngram != other_ngram and set(ngram).issubset(set(other_ngram)):
-                    identical_ngrams.remove(ngram)
-                    break
+                blocks = seq_matcher.get_matching_blocks()[:-1]  # the last block is always empty
+                common_ngrams.extend((ngram_a[block.a: block.a+block.size] for block in blocks))
 
-        return identical_ngrams
+        # Delete common n-grams contained in other common n-grams
+        common_ngrams = sorted(common_ngrams, key=lambda ngram: len(ngram), reverse=True)
+        for i in range(len(common_ngrams)):
+            n_ngrams = len(common_ngrams)
+            if i == n_ngrams:
+                break
+
+            ngram = common_ngrams[i]
+            ngrams_to_pop = (j for j in reversed(range(i+1, n_ngrams)) if self.is_less_precise(common_ngrams[j], ngram))
+            for j in ngrams_to_pop:
+                common_ngrams.pop(j)
+
+        return set(common_ngrams)
 
     def iter_bin_attributes(self, data: list[PatternType]) -> Iterator[tuple[PatternType, fbarray]]:
         """Iterate binary attributes obtained from `data` (from the most general to the most precise ones)
@@ -69,30 +82,38 @@ class NgramsPS(AbstractPS):
         for n_gram, extent in L_extent:
             yield n_gram, extent
 
-
     def is_less_precise(self, a: PatternType, b: PatternType) -> bool:
         """Return True if pattern `a` is less precise than pattern `b`"""
-        for smaller_tuple in b:
+        b_texts_sizes = [(len(ngram), ' '.join(ngram)) for ngram in sorted(b, key=lambda ngram: len(ngram), reverse=True)]
+
+        for smaller_tuple in a:
+            smaller_text = ' '.join(smaller_tuple)
+            smaller_size = len(smaller_tuple)
+
             inclusion_found = False
-            for larger_tuple in a:
-                if any(smaller_tuple == larger_tuple[i:i+len(smaller_tuple)] for i in range(len(larger_tuple) - len(smaller_tuple) + 1)):
+            for larger_size, larger_text in b_texts_sizes:
+                if smaller_size > larger_size:
+                    break
+
+                if smaller_text in larger_text:
                     inclusion_found = True
                     break
+
             if not inclusion_found:
                 return False
+
         return True
 
     def n_bin_attributes(self, data: list[PatternType]) -> int:
         """Count the number of attributes in the binary representation of `data`"""
         count = 0
-        nbr_of_words = 0
-        for i in range(len(data)):
-            nbr_of_words += len(list(list(data[i])[0]))
+        nbr_of_words = len({word for pattern in data for word in pattern})
+
+        # TODO: Check if this is valid formula
         for i in range(nbr_of_words):
             for j in range(i + self.min_n, nbr_of_words + 1):
                 count += 1
-        return(count)
-
+        return count
 
 
 class SynonymsPS(NgramsPS):
