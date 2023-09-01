dbb3fd62e5c18fd7e3c33d3fc3a68fa288ec9665
Egor Dudyrev <egor.dudyrev@loria.fr>
2023-09-01 19:17:01 +0200

    Add basic function to iterate Synonyms binary attributes

--- paspailleur/pattern_structures/nlp_ps.py
+++ paspailleur/pattern_structures/nlp_ps.py
@@ -1,7 +1,6 @@
-import difflib
 from collections import deque
 from functools import reduce
-from itertools import chain
+from itertools import combinations
 from typing import Iterator, Iterable
 
 from bitarray import frozenbitarray as fbarray, bitarray
@@ -11,7 +10,6 @@ from .abstract_ps import AbstractPS
 
 
 import nltk
-from nltk.util import ngrams
 from nltk.corpus import wordnet
 nltk.download('wordnet')
 
@@ -40,11 +38,50 @@ class SynonymsPS(AbstractPS):
 
     def join_patterns(self, a: PatternType, b: PatternType) -> PatternType:
         """Return the common synonyms between both patterns `a` and `b`"""
-        return a & b
+        return a.intersection(b)
 
-    def iter_bin_attributes(self, data: list[PatternType]) -> Iterator[tuple[PatternType, fbarray]]:
-        # TODO: Implement
-        raise NotImplementedError
+    def is_less_precise(self, a: PatternType, b: PatternType) -> bool:
+        return a.issubset(b)
+
+    def iter_bin_attributes(self, data: list[PatternType], min_support: int = 0) -> Iterator[tuple[PatternType, fbarray]]:
+        empty_extent = bazeros(len(data))
+
+        synonyms_extents: dict[str, bitarray] = {}
+        for i, pattern in enumerate(data):
+            for ngram in pattern:
+                for word in ngram:
+                    if word not in synonyms_extents:
+                        synonyms_extents[word] = empty_extent.copy()
+                    synonyms_extents[word][i] = True
+
+        total_pattern = set(synonym for synonym, extent in synonyms_extents.items() if extent.all())
+        yield total_pattern, fbarray(~empty_extent)
+
+        words_to_pop = [word for word, extent in synonyms_extents.items() if extent.count() < min_support or extent.all()]
+        for word in words_to_pop:
+            del synonyms_extents[word]
+
+        synonyms, extents = [list(vals) for vals in zip(*synonyms_extents.items())]
+        n_synonyms = len(synonyms)
+
+        queue = deque([({word}, extent, i) for i, (word, extent) in enumerate(zip(synonyms, extents))])
+        while queue:
+            words, extent, max_word_idx = queue.popleft()
+            yield words, fbarray(extent)
+
+            for i in range(max_word_idx+1, n_synonyms):
+                next_extent = extent & extents[i]
+                if not next_extent.any() or next_extent.count() < min_support:
+                    continue
+
+                if extent & extents[i] == extent or extents[i] & extent == extents[i]:
+                    continue
+
+                next_words = words | {synonyms[i]}
+                queue.append((next_words, next_extent, i))
+
+        if min_support == 0:
+            yield None, fbarray(empty_extent)
 
     def n_bin_attributes(self, data: list[PatternType]) -> int:
         """Count the number of attributes in the binary representation of `data`"""
@@ -94,13 +131,4 @@ class AntonymsPS(NgramPS):
         for i in range(len(data)):
             n += len(list(list(data[i])[0]))
         return (n * (n + 1)) // 2
-
-
-
-
-
-
-
-
-
 
