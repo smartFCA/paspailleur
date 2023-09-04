8544c87bd714eff0dfe75fc7a109051a4fb532d7
Egor Dudyrev <egor.dudyrev@loria.fr>
2023-09-04 18:37:19 +0200

    Make Synonym and Antonym PS children of CnjSetPS (conjunctive set pat. structure)

--- paspailleur/pattern_structures/nlp_ps.py
+++ paspailleur/pattern_structures/nlp_ps.py
@@ -6,7 +6,9 @@ from typing import Iterator, Iterable
 from bitarray import frozenbitarray as fbarray, bitarray
 from bitarray.util import zeros as bazeros
 
-from .abstract_ps import AbstractPS
+#from .abstract_ps import AbstractPS
+from paspailleur.pattern_structures import AbstractPS
+from paspailleur.pattern_structures.set_ps import CnjSetPS
 
 
 import nltk
@@ -14,10 +16,10 @@ from nltk.corpus import wordnet
 nltk.download('wordnet')
 
 
-class SynonymsPS(AbstractPS):
-    PatternType = set[str]  # Every text is described by a set of synonyms to words in the text
+class SynonymsPS(CnjSetPS):
+    PatternType = frozenset[str]  # Every text is described by a set of synonyms to words in the text
     n_synonyms: int | None = 1  # number of synonyms for a word
-    
+
     def __init__(self, n_synonyms: int | None = 1):
         self.n_synonyms = n_synonyms
 
@@ -36,99 +38,32 @@ class SynonymsPS(AbstractPS):
             synonyms = reduce(lambda a, b: a | b, (self.get_synonyms(word) for word in words), set())
             yield synonyms
 
-    def join_patterns(self, a: PatternType, b: PatternType) -> PatternType:
-        """Return the common synonyms between both patterns `a` and `b`"""
-        return a.intersection(b)
 
-    def is_less_precise(self, a: PatternType, b: PatternType) -> bool:
-        return a.issubset(b)
-
-    def iter_bin_attributes(self, data: list[PatternType], min_support: int = 0) -> Iterator[tuple[PatternType, fbarray]]:
-        empty_extent = bazeros(len(data))
-
-        synonyms_extents: dict[str, bitarray] = {}
-        for i, pattern in enumerate(data):
-            for ngram in pattern:
-                for word in ngram:
-                    if word not in synonyms_extents:
-                        synonyms_extents[word] = empty_extent.copy()
-                    synonyms_extents[word][i] = True
-
-        total_pattern = set(synonym for synonym, extent in synonyms_extents.items() if extent.all())
-        yield total_pattern, fbarray(~empty_extent)
-
-        words_to_pop = [word for word, extent in synonyms_extents.items() if extent.count() < min_support or extent.all()]
-        for word in words_to_pop:
-            del synonyms_extents[word]
-
-        synonyms, extents = [list(vals) for vals in zip(*synonyms_extents.items())]
-        n_synonyms = len(synonyms)
-
-        queue = deque([({word}, extent, i) for i, (word, extent) in enumerate(zip(synonyms, extents))])
-        while queue:
-            words, extent, max_word_idx = queue.popleft()
-            yield words, fbarray(extent)
-
-            for i in range(max_word_idx+1, n_synonyms):
-                next_extent = extent & extents[i]
-                if not next_extent.any() or next_extent.count() < min_support:
-                    continue
-
-                if extent & extents[i] == extent or extents[i] & extent == extents[i]:
-                    continue
-
-                next_words = words | {synonyms[i]}
-                queue.append((next_words, next_extent, i))
-
-        if min_support == 0:
-            yield None, fbarray(empty_extent)
-
-    def n_bin_attributes(self, data: list[PatternType]) -> int:
-        """Count the number of attributes in the binary representation of `data`"""
-        # TODO: Test if this equation works
-        n = sum(len(pattern) for pattern in data)
-        return (n * (n + 1)) // 2
-
-
-class AntonymsPS(NgramPS):
-    PatternType = set[tuple[str]]
-    num_antonyms : int
+class AntonymsPS(CnjSetPS):
+    PatternType = frozenset[str]  # Every text is described by a set of antonyms to words in the text
+    num_antonyms : int | None = 1  # number of antonyms for a word
     
-    def __init__(self, num_antonyms = 1):
+    def __init__(self, num_antonyms: int = 1):
         self.num_antonyms = num_antonyms
 
-    def get_antonyms(self, word):
+    def get_antonyms(self, word: str) -> PatternType:
         antonyms = set()
         for synset in wordnet.synsets(word):
             for lemma in synset.lemmas():
                 if lemma.antonyms():
                     antonyms.add(lemma.antonyms()[0].name())
                     if self.num_antonyms is not None and len(antonyms) >= self.num_antonyms:
-                        return antonyms
-        return antonyms
+                        return frozenset(antonyms)
+        return frozenset(antonyms)
+
+    def preprocess_data(self, data: Iterable[str], separator=' ') -> Iterator[PatternType]:
+        for text in data:
+            words = text.split(separator)
+            antonyms = reduce(lambda a, b: a | b, (self.get_antonyms(word) for word in words), set())
+            yield antonyms
 
 
-    def get_antonyms_set_for_text(self, a: PatternType) -> PatternType:
-      antonyms_list = []
-      words_list = list(a)[0]
-      for i in range(len(words_list)):
-          for antonym in list(self.get_antonyms(words_list[i], self.num_antonyms)):
-            antonyms_list.append(antonym)
-      antonyms_tuple = [tuple(antonyms_list)]
-      return(set(antonyms_tuple))
-
-
-    def join_antonyms_patterns(self, a: PatternType, b: PatternType) -> PatternType:
-        """Return the common antonyms between both patterns `a` and `b`"""
-        antonyms_text1 = self.get_antonyms_set_for_text(a, self.num_antonyms)
-        antonyms_text2 = self.get_antonyms_set_for_text(b, self.num_antonyms)
-        return(self.join_patterns(antonyms_text1, antonyms_text2))
-
-
-    def n_bin_attributes(self, data: list[PatternType]) -> int:
-        """Count the number of attributes in the binary representation of `data`"""
-        n = 0
-        for i in range(len(data)):
-            n += len(list(list(data[i])[0]))
-        return (n * (n + 1)) // 2
+if __name__ == '__main__':
+    syn_ps = SynonymsPS(n_synonyms=5)
+    assert syn_ps.get_synonyms('great') == {'outstanding', 'bang-up', 'great', 'bully', 'corking'}
 
--- paspailleur/pattern_structures/set_ps.py
+++ paspailleur/pattern_structures/set_ps.py
@@ -1,9 +1,12 @@
-from typing import Iterator, Optional
-from bitarray import frozenbitarray as fbarray
+from collections import deque
+from typing import Iterator, Optional, TypeVar
+from bitarray import frozenbitarray as fbarray, bitarray
+from bitarray.util import zeros as bazeros
 from .abstract_ps import AbstractPS
 
 from itertools import combinations
 
+T = TypeVar('T')
 
 class SetPS(AbstractPS):
     PatternType = frozenset
@@ -36,6 +39,76 @@ class SetPS(AbstractPS):
     def is_less_precise(self, a: PatternType, b: PatternType) -> bool:
         """Return True if pattern `a` is less precise than pattern `b`"""
         return a & b == b
+
+    def n_bin_attributes(self, data: list[PatternType]) -> int:
+        """Count the number of attributes in the binary representation of `data`"""
+        unique_values = set()
+        for data_row in data:
+            unique_values |= data_row
+        return 2**len(unique_values)
+
+
+DsjSetPS = SetPS
+
+
+class CnjSetPS(AbstractPS):
+    PatternType = frozenset[T]
+    bottom = None
+
+    def join_patterns(self, a: PatternType, b: PatternType) -> PatternType:
+        """Return the most precise common pattern, describing both patterns `a` and `b`"""
+        return a.intersection(b)
+
+    def is_less_precise(self, a: PatternType, b: PatternType) -> bool:
+        """Return True if pattern `a` is less precise than pattern `b`"""
+        return a.issubset(b)
+
+    def iter_bin_attributes(self, data: list[PatternType], min_support: int = 1)\
+            -> Iterator[tuple[PatternType, fbarray]]:
+        """Iterate binary attributes obtained from `data` (from the most general to the most precise ones)
+
+        :parameter
+            data: list[PatternType]
+             list of object descriptions
+        :return
+            iterator of (description: PatternType, extent of the description: frozenbitarray)
+        """
+        empty_extent = bazeros(len(data))
+        vals_extents: dict[T, bitarray] = {}
+        for i, pattern in enumerate(data):
+            for v in pattern:
+                if v not in vals_extents:
+                    vals_extents[v] = empty_extent.copy()
+                vals_extents[v][i] = True
+
+        total_pattern = {v for v, extent in vals_extents.items() if extent.all()}
+        yield frozenset(total_pattern), fbarray(~empty_extent)
+
+        vals_to_pop = (v for v, extent in vals_extents.items() if extent.count() < min_support or extent.all())
+        for v in vals_to_pop:
+            del vals_extents[v]
+
+        vals, extents = [list(vals) for vals in zip(*vals_extents.items())]
+        n_vals = len(vals)
+
+        queue = deque([({v}, extent, i) for i, (v, extent) in enumerate(zip(vals, extents))])
+        while queue:
+            words, extent, max_val_id = queue.popleft()
+            yield frozenset(words), fbarray(extent)
+
+            for i in range(max_val_id + 1, n_vals):
+                next_extent = extent & extents[i]
+                if not next_extent.any() or next_extent.count() < min_support:
+                    continue
+
+                if extent & extents[i] == extent or extents[i] & extent == extents[i]:
+                    continue
+
+                next_words = words | {vals[i]}
+                queue.append((next_words, next_extent, i))
+
+        if min_support == 0:
+            yield None, fbarray(empty_extent)
 
     def n_bin_attributes(self, data: list[PatternType]) -> int:
         """Count the number of attributes in the binary representation of `data`"""
