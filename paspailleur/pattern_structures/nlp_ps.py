9a2523426396f9d003766a5fd2297a05efe450cb
Egor Dudyrev <egor.dudyrev@loria.fr>
2023-09-05 14:46:52 +0200

    Add tests for AntonymPS and SynonymPS

--- paspailleur/pattern_structures/nlp_ps.py
+++ paspailleur/pattern_structures/nlp_ps.py
@@ -9,7 +9,7 @@ from nltk.corpus import wordnet
 nltk.download('wordnet')
 
 
-class SynonymsPS(SubSetPS):
+class SynonymPS(SubSetPS):
     PatternType = frozenset[str]  # Every text is described by a set of synonyms to words in the text
     n_synonyms: int | None = 1  # number of synonyms for a word
 
@@ -32,12 +32,12 @@ class SynonymsPS(SubSetPS):
             yield synonyms
 
 
-class AntonymsPS(SubSetPS):
+class AntonymPS(SubSetPS):
     PatternType = frozenset[str]  # Every text is described by a set of antonyms to words in the text
-    num_antonyms : int | None = 1  # number of antonyms for a word
+    n_antonyms: int | None = 1  # number of antonyms for a word
     
-    def __init__(self, num_antonyms: int = 1):
-        self.num_antonyms = num_antonyms
+    def __init__(self, n_antonyms: int = 1):
+        self.n_antonyms = n_antonyms
 
     def get_antonyms(self, word: str) -> PatternType:
         antonyms = set()
@@ -45,7 +45,7 @@ class AntonymsPS(SubSetPS):
             for lemma in synset.lemmas():
                 if lemma.antonyms():
                     antonyms.add(lemma.antonyms()[0].name())
-                    if self.num_antonyms is not None and len(antonyms) >= self.num_antonyms:
+                    if self.n_antonyms is not None and len(antonyms) >= self.n_antonyms:
                         return frozenset(antonyms)
         return frozenset(antonyms)
 
@@ -54,9 +54,3 @@ class AntonymsPS(SubSetPS):
             words = text.split(separator)
             antonyms = reduce(lambda a, b: a | b, (self.get_antonyms(word) for word in words), set())
             yield antonyms
-
-
-if __name__ == '__main__':
-    syn_ps = SynonymsPS(n_synonyms=5)
-    assert syn_ps.get_synonyms('great') == {'outstanding', 'bang-up', 'great', 'bully', 'corking'}
-
