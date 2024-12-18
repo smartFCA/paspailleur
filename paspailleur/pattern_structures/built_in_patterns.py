a242717da38a421331e5c2674700193f6c679ee3
Egor Dudyrev <egor.dudyrev@loria.fr>
2024-12-18 18:03:30 +0100

    Design basic IntervalPattern

--- paspailleur/pattern_structures/built_in_patterns.py
+++ paspailleur/pattern_structures/built_in_patterns.py
@@ -1,3 +1,5 @@
+from typing import Self, Union
+
 from .pattern import Pattern
 
 
@@ -13,3 +15,77 @@ class ItemSetPattern(Pattern):
 
     def __repr__(self) -> str:
         return f"ItemSetPattern({self.value})"
+
+
+class IntervalPattern(Pattern):
+    # PatternValue semantics: ((lower_bound, is_closed), (upper_bound, is_closed))
+    PatternValueType = tuple[tuple[float, bool], tuple[float, bool]]
+
+    def __init__(self, value: Union[PatternValueType, str]):
+        super().__init__(None)
+        if isinstance(value, str):
+            lb, ub = map(str.strip, value[1:-1].replace('∞', 'inf').split(','))
+            closed_lb, closed_ub = value[0] == '[', value[-1] == ']'
+        else:
+            lb, closed_lb = value[0]
+            ub, closed_ub = value[1]
+        self._lower_bound = float(lb)
+        self._upper_bound = float(ub)
+        self._is_closed_lower_bound = bool(closed_lb)
+        self._is_closed_upper_bound = bool(closed_ub)
+
+    @property
+    def value(self) -> PatternValueType:
+        return (self._lower_bound, self._is_closed_lower_bound), (self._upper_bound, self._is_closed_upper_bound)
+
+    def __repr__(self) -> str:
+        lbound_sign = '[' if self._is_closed_lower_bound else '('
+        ubound_sign = ']' if self._is_closed_upper_bound else ')'
+        return f"IntervalPattern({lbound_sign}{self._lower_bound}, {self._upper_bound}{ubound_sign})"
+
+    def __and__(self, other: Self) -> Self:
+        """Return self & other, i.e. the most precise pattern that is less precise than both self and other"""
+        if self._lower_bound < other._lower_bound:
+            lbound, closed_lb = self._lower_bound, self._is_closed_lower_bound
+        elif other._lower_bound < self._lower_bound:
+            lbound, closed_lb = other._lower_bound, other._is_closed_lower_bound
+        else:  # self._lower_bound == other._lower_bound
+            lbound = self._lower_bound
+            closed_lb = self._is_closed_lower_bound or other._is_closed_lower_bound
+
+        if self._upper_bound > other._upper_bound:
+            ubound, closed_ub = self._upper_bound, self._is_closed_upper_bound
+        elif other._upper_bound > self._upper_bound:
+            ubound, closed_ub = other._upper_bound, other._is_closed_upper_bound
+        else:  # self._upper_bound == other._upper_bound
+            ubound = self._upper_bound
+            closed_ub = self._is_closed_upper_bound or other._is_closed_upper_bound
+
+        new_value = (lbound, closed_lb), (ubound, closed_ub)
+        return self.__class__(new_value)
+
+    def __or__(self, other: Self) -> Self:
+        """Return self | other, i.e. the least precise pattern that is more precise than both self and other"""
+        if self._lower_bound < other._lower_bound:
+            lbound, closed_lb = other._lower_bound, other._is_closed_lower_bound
+        elif other._lower_bound < self._lower_bound:
+            lbound, closed_lb = self._lower_bound, self._is_closed_lower_bound
+        else:  # self._lower_bound == other._lower_bound
+            lbound = self._lower_bound
+            closed_lb = self._is_closed_lower_bound and other._is_closed_lower_bound
+
+        if self._upper_bound > other._upper_bound:
+            ubound, closed_ub = other._upper_bound, other._is_closed_upper_bound
+        elif other._upper_bound > self._upper_bound:
+            ubound, closed_ub = self._upper_bound, self._is_closed_upper_bound
+        else:  # self._upper_bound == other._upper_bound
+            ubound = self._upper_bound
+            closed_ub = self._is_closed_upper_bound and other._is_closed_upper_bound
+
+        new_value = (lbound, closed_lb), (ubound, closed_ub)
+        return self.__class__(new_value)
+
+    def __sub__(self, other: Self) -> Self:
+        """Return self - other, i.e. the least precise pattern s.t. (self-other)|other == self"""
+        # TODO: Find out how to implement this. And should it be implemented
+        raise NotImplementedError
--- tests/test_pattern_structures/test_built_in_patterns.py
+++ tests/test_pattern_structures/test_built_in_patterns.py
@@ -1,3 +1,5 @@
+import math
+
 from paspailleur.pattern_structures import built_in_patterns as bip
 
 
@@ -36,3 +38,33 @@ def test_ItemSetPattern():
     b = bip.ItemSetPattern({3, 4, 5, 6, 7})
     sub = bip.ItemSetPattern({1, 2})
     assert a - b == sub
+
+
+def test_IntervalPattern():
+    a = bip.IntervalPattern(((1, True), (10, False)))
+    a2 = bip.IntervalPattern('[1, 10)')
+    assert a.value == ((1, True), (10, False))
+    assert a == a2
+
+    a = bip.IntervalPattern('[-inf, ∞)')
+    assert a._lower_bound == -math.inf
+    assert a._upper_bound == math.inf
+
+    a = bip.IntervalPattern('[1, 10]')
+    b = bip.IntervalPattern('[1, 20]')
+    assert a & b == b
+    assert a | b == a
+    assert a >= b
+    assert a.issuperpattern(b)
+    assert b <= a
+    assert b.issubpattern(a)
+    assert not a.issubpattern(b)
+
+    a = bip.IntervalPattern('[1, 10)')
+    b = bip.IntervalPattern('(1, 10]')
+    meet = bip.IntervalPattern('[1, 10]')
+    join = bip.IntervalPattern('(1, 10)')
+    assert a & b == meet
+    assert a | b == join
+
+    assert str(a) == 'IntervalPattern([1.0, 10.0))'
