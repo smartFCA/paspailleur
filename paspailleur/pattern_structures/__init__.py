66d0a82af03783362db7a344245af5db080b8664
Egor Dudyrev <egor.dudyrev@loria.fr>
2025-02-05 18:06:54 +0100

    Delete the whole old codebase as it is no longer needed. These are breaking changes that make every old paspailleur-based code no longer runnable. So I have updated the package minor version number to reflect this change

--- paspailleur/pattern_structures/__init__.py
+++
@@ -1,7 +0,0 @@
-from .abstract_ps import AbstractPS
-from .set_ps import DisjunctiveSetPS, ConjunctiveSetPS
-from .interval_ps import IntervalPS, BoundStatus
-from .cartesian_ps import CartesianPS
-
-from .ngram_ps import NgramPS
-from .nlp_ps import SynonymPS, AntonymPS
