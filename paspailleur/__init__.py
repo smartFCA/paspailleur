e4fc05f883dcb055aa6bbabc19887d7f675dab1a
Egor Dudyrev <egor.dudyrev@loria.fr>
2025-06-16 22:56:08 +0200

    Add shortcuts for Pattern, PatternStructure and other classes to __init__ file of the package

--- paspailleur/__init__.py
+++ paspailleur/__init__.py
@@ -1,1 +1,6 @@
+from paspailleur.pattern_structures.pattern import Pattern
+from paspailleur.pattern_structures.pattern_structure import PatternStructure
+from paspailleur.pattern_structures import built_in_patterns as bip
+from paspailleur.pattern_structures.pattern_factory import pattern_factory
+
 __version__ = '0.1.0'
